703002ad1373e466ec0085d8cb588e0d6661e8bd
babrovka <babrovka@gmail.com>
2012-12-09 15:04:49 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Polisoms::Application.config.secret_token = '94feb57ff3768b722750b55464df5f5b6f3182ec8591ef122b2c087735a04c5d64e05f2357e74c092f4cd4e2804243f808877a301ed2cb9e05386788b710f079'
