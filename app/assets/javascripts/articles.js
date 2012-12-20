998634e9ef99e2e63b3f87908c3ff57619fd380a
HeeL <parizhskiy@gmail.com>
2012-12-20 14:01:44 +0100

    ajax search on checkbox change, removed submit btw

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -1,19 +1,22 @@
-jQuery(function(){ 
+$(document).ready(function(){
+
+  $('#sort_form').bind('ajax:success', function(data, response, xhr){
+    $('#news_block').html(response);
+  });
   
   $("#radio1").click(function(){
     $(".left__news-check").attr('checked', null);
-	//$('form').submit();
+	  $('#sort_form').submit();
   });
 
-$(".left__news-check").click(function(){
+  $(".left__news-check").click(function(){
     $("#radio2").attr('checked', 'checked');
-	//$('form').submit();
+	  $('#sort_form').submit();
   });
 
   $("#radio2").click(function(){
     $(".left__news-check:first").attr('checked', 'checked');
-	//$('form').submit();
+	  $('#sort_form').submit();
   });
 
-  });
-
+});
