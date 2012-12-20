005caab49db18c212eb0467d5898e9209f2790e1
babrovka <babrovka@gmail.com>
2012-12-20 14:02:46 +0400

    submit

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -2,17 +2,17 @@ jQuery(function(){
 
   $("#radio1").click(function(){
     $(".left__news-check").attr('checked', null);
-	$('ul.news').load('/articles', $("#form").serializeArray())
+	$('form').submit();
   });
 
 $(".left__news-check").click(function(){
     $("#radio2").attr('checked', 'checked');
-	$('ul.news').load('/articles', $("#form").serializeArray())
+	$('form').submit();
   });
 
   $("#radio2").click(function(){
     $(".left__news-check:first").attr('checked', 'checked');
-	$('ul.news').load('/articles', $("#form").serializeArray())
+	$('form').submit();
   });
 
   });
