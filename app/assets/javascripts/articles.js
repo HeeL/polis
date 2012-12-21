a53351638efc20020548dbde8ccb36014a1f40c7
babrovka <babrovka@gmail.com>
2012-12-21 14:23:01 +0400

    sorting with active element

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -5,16 +5,22 @@ $(document).ready(function(){
   });
   
   $("#radio1").click(function(){
+	$("#radio1").parent().addClass("left__news-selected");
+	$("#radio2").parent().removeClass("left__news-selected");
     $(".left__news-check").attr('checked', null);
 	  $('#sort_form').submit();
   });
 
   $(".left__news-check").click(function(){
+	$("#radio2").parent().addClass("left__news-selected");
+	$("#radio1").parent().removeClass("left__news-selected");
     $("#radio2").attr('checked', 'checked');
 	  $('#sort_form').submit();
   });
 
   $("#radio2").click(function(){
+	$("#radio2").parent().addClass("left__news-selected");
+	$("#radio1").parent().removeClass("left__news-selected");
     $(".left__news-check:first").attr('checked', 'checked');
 	  $('#sort_form').submit();
   });
