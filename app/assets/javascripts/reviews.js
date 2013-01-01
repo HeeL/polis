0cdf12a47a84789b0b4cbd693b5c66c46368e23f
HeeL <parizhskiy@gmail.com>
2013-01-01 15:08:37 +0100

    make a string out of docs ids

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
 			$(v).find('option[value='+ $(el).find('option:selected').val() +']').remove()
 		});
 	});
-	$('.doctors-select:last').attr('name', 'review[0][doctor_id]')
+	$('.doctors-select:last').attr('name', 'review_doc[0][doctor_id]')
 	
 	$('#add_doctors').bind('click', function() {
 		last_select = $('.doctors-select:last');
@@ -21,13 +21,20 @@ $(document).ready(function() {
 		$('#doc').append('<div class="doc" data-num="'+ num +'">' + $('.doc:last').html() + '</div>');
 		$('#doc').css('height', ($('.doctors-select').length - 1) * 30 + 50 + 'px')
 		$('.doc').css('margin-top', '10px');
-		$('.doctors-select:last').attr('name', 'review['+ num +'][doctor_id]')
+		$('.doctors-select:last').attr('name', 'review_doc['+ num +'][doctor_id]')
 		$('.doctors-select:last').change();
 		$('.doctors-select:last option[value='+ last_select.val() +']').remove();
 		if($('.doctors-select:last option').length == 1) {
 			$(this).hide();
 		}
 		return false;
+	});
+
+	$('#new_review, #edit_review').bind('submit', function() {
+		docs = '';
+		$('.doctors-select option:selected').each(function(k,v){docs += $(v).val() +  ','});
+		docs = docs.slice(0, -1);
+		$('#review_docs').val(docs)
 	});
 
 	$(".response__menu-up, .i2, #blago").click(function(){
