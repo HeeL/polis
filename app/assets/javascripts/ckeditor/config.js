b3b2c77249063f2afb05b611c115adb6d4eb6930
Viacheslav Bobrov <babrovka@gmail.com>
2013-01-08 21:50:06 +0400

    text and title styles

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -41,10 +41,7 @@ config.stylesSet = 'my_styles';
 CKEDITOR.stylesSet.add( 'my_styles',
 [
     // Block-level styles
-    { name : 'Blue Title', element : 'h2', styles : { 'color' : 'Blue' } },
-    { name : 'Red Title' , element : 'h3', styles : { 'color' : 'Red' } },
- 
-    // Inline styles
-    { name : 'CSS Style', element : 'span', attributes : { 'class' : 'my_style' } },
-    { name : 'Marker: Yellow', element : 'span', styles : { 'background-color' : 'Yellow' } }
+    { name : 'Заголовок', element : 'h2', styles : { 'color' : 'Blue' } },
+    { name : 'Текст' , element : 'p', styles : { 'color' : '#797C80' } },
+
 ]);
