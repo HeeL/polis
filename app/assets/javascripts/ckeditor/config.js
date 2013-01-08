9735785f2601e8c81e7ae21e4eb71e37cd2534a4
Viacheslav Bobrov <babrovka@gmail.com>
2013-01-08 19:16:33 +0400

    ckeditor style test

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -32,4 +32,19 @@ CKEDITOR.editorConfig = function( config )
     config.filebrowserUploadUrl = "/ckeditor/attachment_files";
 
 
+config.extraPlugins = 'stylesheetparser';
+
+config.contentsCss = ['/contents.css'];
+config.stylesSet = 'my_styles';
 };
+
+CKEDITOR.stylesSet.add( 'my_styles',
+[
+    // Block-level styles
+    { name : 'Blue Title', element : 'h2', styles : { 'color' : 'Blue' } },
+    { name : 'Red Title' , element : 'h3', styles : { 'color' : 'Red' } },
+ 
+    // Inline styles
+    { name : 'CSS Style', element : 'span', attributes : { 'class' : 'my_style' } },
+    { name : 'Marker: Yellow', element : 'span', styles : { 'background-color' : 'Yellow' } }
+]);
