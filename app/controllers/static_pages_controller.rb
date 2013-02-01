7cd172b046a31d56c5e8cfb45351ee675951a743
Viacheslav Bobrov <babrovka@gmail.com>
2013-02-01 19:58:08 +0400

    home links correction

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,10 +3,5 @@ class StaticPagesController < ApplicationController
     @review = Review.new
     @office_select = Office.all
     @articles = Article.limit(3)
-    @priem = About.find(15)
-    @zapis = About.find(16)
-    @netpolis = Service.find(9)
-    @whyfree = About.find(18)
-    @whois = About.find(14)
   end
 end
