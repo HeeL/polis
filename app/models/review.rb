b4d061012c6e44a98cb50be338a830228c35b631
Viacheslav Bobrov <babrovka@gmail.com>
2013-03-25 17:54:05 +0400

    doctors partial

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,5 @@ class Review < ActiveRecord::Base
   attr_accessible :author, :title, :contact, :description, :doctor_id, :office_id, :status, :check, :doctors
   belongs_to :doctor
   belongs_to :office
+  
 end
